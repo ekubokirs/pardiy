96bc882ba77bedfa7964c1c6b4fc8442574b3d60
Emilia Kubo Kirschenbaum <ekubokirs@gmail.com>
2013-07-22 18:13:50 -0700

    updated edit.html.erb and new.html.erb

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,13 +3,12 @@ class SessionController < ApplicationController
   end
 
   def create
-  	puts params[:password]
     if params[:password].blank?
       user = User.find_by(email: params[:email])
 
       if user
         user.code = SecureRandom.urlsafe_base64
-        user.expires_at = Time.now + 2. hours
+        user.expires_at = Time.now + 48. hours
         user.save
 
         PasswordMailer.reset_email(user).deliver
@@ -20,7 +19,6 @@ class SessionController < ApplicationController
     else
       user = User.authenticate(params[:email], params[:password])
 
-      puts user
       if user
         session[:user_id] = user.id
         redirect_to root_url, alert: "You've Logged In!" and return
