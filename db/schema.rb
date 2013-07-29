54e217bf5ac91dfe0045a2d719ff137f3f26328b
Emilia Kubo Kirschenbaum <ekubokirs@gmail.com>
2013-07-29 10:31:46 -0700

    updated edit events

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130725232832) do
+ActiveRecord::Schema.define(version: 20130729170259) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,11 @@ ActiveRecord::Schema.define(version: 20130725232832) do
     t.string   "venue"
     t.string   "event_type"
     t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "photos", force: true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
